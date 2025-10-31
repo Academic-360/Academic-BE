bae6fc883b6dbe02e4075a1ff685456ccf6dafc4
Ayesh Senavirathne <77045415+ayeshks@users.noreply.github.com>
2025-10-31 11:46:33 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,6 +1,3 @@
 import app from "../src/server.js";
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
-
+export default app;
--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import connectDB from "../config/db.js";
-import authRoutes from "../routes/authRoutes.js";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
